de263459b3cf8cb77a792babd494953f31b3d6bc
Avinash Agarwal <avinasha39@users.noreply.github.com>
2022-07-18 13:02:44 +0530

    Refactored

--- C#_Class.cs
+++ C#_Class.cs
@@ -21,6 +21,37 @@ class Checker
 // 1. Method doing handling multiple checks breaking SRP 
 // 2. Method is closed for extension and has to be modified in future. 
 
+//Refacored Code
+class Checker
+{
+    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
+        return isTemperatureOk(temperature) && isSOCOk(soc) && ischargeRateOk(chargeRate);
+    }
+    
+    public bool isTemperatureOk(float temperature){
+        if(temperature < 0 || temperature > 45) {
+            Console.WriteLine("Temperature is out of range!");
+            return false;
+        }
+        return true;
+    }
+    
+    public bool isSOCOk(float soc){
+        if(soc < 20 || soc > 80) {
+            Console.WriteLine("State of Charge is out of range!");
+            return false;
+        }
+        return true;
+    }
+    public bool ischargeRateOk(float chargeRate){
+         if(chargeRate > 0.8) {
+            Console.WriteLine("Charge Rate is out of range!");
+            return false;
+        }
+        return true;
+    }
+}
+
 // Fixes
 // 1. Adding 3 different method to handle 3 responsiblity and retuen boolean
 // 2. batteryIsOk should call different method, consolidate the result and return
